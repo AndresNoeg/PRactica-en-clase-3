9928abdca82695e7c61c5d8a7a5c8c3aea3e56f9
AndresNoeg <a.23308051281184@cbtis128.edu.mx>
2024-09-23 20:50:13 -0600

    Practica 3 Farenheit a celsius

---
+++ Farenheit_a_celsius.py
@@ -0,0 +1,15 @@
+print (" ")
+print ("Andres Noe Gomez Garcia Farenheit a celsius")
+print(" ")
+
+#Pide la temperatura en farenheit
+faren=float(input("ingrese la temperatura en Farenheit"))
+
+#Formula para sacar celsius
+r= 5/9
+r1= faren - 32
+cel=r*r1
+
+print(faren,"°F es igual a: ", cel, "°C")
+print(" ")
+
